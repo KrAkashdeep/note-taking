962a08024e53f1f74a6899df18d9182245bd5cf8
KrAkashdeep <akashdeep93140@gmail.com>
2025-02-12 23:19:45 +0530

    edited

--- frontend/src/components/AddTextNote.jsx
+++ frontend/src/components/AddTextNote.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 
-
 import { useState } from "react";
 
 const AddTextNote = ({ onClose, onAddNote }) => {
@@ -23,7 +22,10 @@ const AddTextNote = ({ onClose, onAddNote }) => {
       <div className="bg-white border-2 rounded-lg p-6 w-full max-w-md">
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-xl font-semibold">Add Text Note</h2>
-          <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
+          <button
+            onClick={onClose}
+            className="text-gray-500 hover:text-gray-700"
+          >
             ✕
           </button>
         </div>
